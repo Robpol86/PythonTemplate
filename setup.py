f725573fef66dbb2f0eb0b695e75e7150d842524
Robpol86 <robpol86@gmail.com>
2015-03-23 00:53:24 -0700

    flake8-import-order, robpol86-pytest-ipdb
    Testing imports order in flake8 tox env.
    Including ipdb in 2.7+ pythons.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import atexit
-from codecs import open
-from distutils.spawn import find_executable
 import os
 import re
-import sys
 import subprocess
+import sys
+from codecs import open
+from distutils.spawn import find_executable
 
 import setuptools.command.sdist
 from setuptools.command.test import test
@@ -66,9 +66,10 @@ class PyTest(test):
 
 
 class PyTestPdb(PyTest):
-    description = 'Run all tests, drops to ipdb upon unhandled exception.'
+    _ipdb = 'ipdb' if sys.version_info[:2] > (2, 6) else 'pdb'
+    description = 'Run all tests, drops to {0} upon unhandled exception.'.format(_ipdb)
     CMD = 'testpdb'
-    TEST_ARGS = ['--ipdb', 'tests']
+    TEST_ARGS = ['--{0}'.format(_ipdb), 'tests']
 
 
 class PyTestCovWeb(PyTest):
