e2d9aae2c1b52a5380f5b5ea96691dcac04c0dfc
Robpol86 <robpol86@gmail.com>
2015-05-30 20:42:03 -0700

    Moved pdb/etc to tox. Pinning flake8 lib.
    flake8-import-order 0.6 behaves differently. Pinning to 0.5.
    Something changed with tox 2.0. Clearing deps in check env.
    Fixing Windows paths in setup.py by using relative path instead of
    absolute path.
    usedevelop for pytest coverage.

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 """Setup script for the project."""
 
-import atexit
 import codecs
 import os
 import re
-import subprocess
-import sys
-from distutils.spawn import find_executable
 
 import setuptools
-from setuptools.command.test import test
 
 _PACKAGES = lambda: [os.path.join(r, s) for r, d, _ in os.walk(NAME_FILE) for s in d if s != '__pycache__']
 _VERSION_RE = re.compile(r"^__(version|author|license)__ = '([\w\.@]+)'$", re.MULTILINE)
@@ -57,56 +52,9 @@ def _safe_read(path, length):
     return contents
 
 
-class PyTest(test):
-    """Run tests with pytest."""
-
-    description = 'Run all tests.'
-    user_options = []
-    CMD = 'test'
-    TEST_ARGS = ['--cov-report', 'term-missing', '--cov', NAME_FILE, 'tests']
-
-    def finalize_options(self):
-        """Finalize options."""
-        overflow_args = sys.argv[sys.argv.index(self.CMD) + 1:]
-        test.finalize_options(self)
-        setattr(self, 'test_args', self.TEST_ARGS + overflow_args)
-        setattr(self, 'test_suite', True)
-
-    def run_tests(self):
-        """Run the tests."""
-        # Import here, cause outside the eggs aren't loaded.
-        pytest = __import__('pytest')
-        err_no = pytest.main(self.test_args)
-        sys.exit(err_no)
-
-
-class PyTestPdb(PyTest):
-    """Run tests with pytest and drop to debugger on test failure/errors."""
-
-    _ipdb = 'ipdb' if sys.version_info[:2] > (2, 6) else 'pdb'
-    description = 'Run all tests, drops to {0} upon unhandled exception.'.format(_ipdb)
-    CMD = 'testpdb'
-    TEST_ARGS = ['--{0}'.format(_ipdb), 'tests']
-
-
-class PyTestCovWeb(PyTest):
-    """Run the tests and open a web browser (OS X only) showing coverage information."""
-
-    description = 'Generates HTML report on test coverage.'
-    CMD = 'testcovweb'
-    TEST_ARGS = ['--cov-report', 'html', '--cov', NAME_FILE, 'tests']
-
-    def run_tests(self):
-        """Run the tests and then open."""
-        if find_executable('open'):
-            atexit.register(lambda: subprocess.call(['open', os.path.join(HERE, 'htmlcov', 'index.html')]))
-        PyTest.run_tests(self)
-
-
 ALL_DATA = dict(
     author_email='robpol86@gmail.com',
     classifiers=CLASSIFIERS,
-    cmdclass={PyTest.CMD: PyTest, PyTestPdb.CMD: PyTestPdb, PyTestCovWeb.CMD: PyTestCovWeb},
     description=DESCRIPTION,
     install_requires=REQUIRES_INSTALL,
     keywords=KEYWORDS,
