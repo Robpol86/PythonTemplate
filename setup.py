083cfd0eb6a127028dc8733a3534ca85576f5f7f
Robpol86 <robpol86@gmail.com>
2016-01-22 20:06:00 -0800

    readme()
    Function is only used for README files. No need to make it fancy.
    
    Private classifier by default so I don't accidentally copy/paste
    blindly.

--- setup.py
+++ setup.py
@@ -6,18 +6,17 @@ from __future__ import print_function
 import codecs
 import os
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
-def safe_read(path):
-    """Try to read file or return empty string if failed.
-
-    :param str path: Relative file path to read.
+def readme():
+    """Try to read README.rst or return empty string if failed.
 
     :return: File contents.
     :rtype: str
     """
-    abspath, file_handle = os.path.join(os.path.abspath(os.path.dirname(__file__)), path), None
+    abspath = os.path.realpath(os.path.join(os.path.dirname(__file__), 'README.rst'))
+    file_handle = None
     try:
         file_handle = codecs.open(abspath, encoding='utf-8')
         return file_handle.read(131072)
@@ -30,33 +29,14 @@ def safe_read(path):
 setup(
     author='@Robpol86',
     author_email='robpol86@gmail.com',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Environment :: MacOS X',
-        'Environment :: Win32 (MS Windows)',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Terminals',
-        'Topic :: Text Processing :: Markup',
-    ],
+    classifiers=['Private :: Do Not Upload'],
     description='This is the template I use for my Python projects.',
     install_requires=[],
     keywords='replace me',
     license='MIT',
-    long_description=safe_read('README.rst'),
+    long_description=readme(),
     name='replace_me',
-    packages=['replace_me'],
+    packages=find_packages(exclude=['tests']),
     url='https://github.com/Robpol86/replace_me',
     version='0.0.1',
     zip_safe=True,
